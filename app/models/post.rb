b6d8e75c18d1a48955faaa7de63dde0587e6f0fd
Abdul Hadi Jafari <abdulhadijafari2015@gmail.com>
2022-08-13 22:26:15 +0430

    Validate post

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,15 +3,18 @@ class Post < ApplicationRecord
   has_many :comments
   has_many :likes
 
+  validates :title, presence: true, length: { maximum: 250 }
+  validates :comments_counter, :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+
   after_save :post_count
+
+  def last_5_comments
+    comments.order(created_at: :desc).limit(5)
+  end
 
   private
 
   def post_count
     author.increment!(:posts_counter)
-  end
-
-  def last_5_comments
-    comments.order(created_at: :desc).limit(5)
   end
 end

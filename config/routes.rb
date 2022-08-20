c699ff5b61bf77ccfa11556e944bbfa1878e8ada
Abdul Hadi Jafari <abdulhadijafari2015@gmail.com>
2022-08-20 10:39:09 +0430

    Fix route

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
-  get "/", to: "main#index"
+  root to: "main#index"
 
   resources :users, only: [:index, :show] do
-    resources :posts, only: [:index, :show] do
-      resources :comments, only: [:index, :show]
+    resources :posts, only: [:index, :show, :new, :create] do
+      resources :comments, only: [:new, :create]
+      resources :likes, only: [:create]
     end
   end
 end

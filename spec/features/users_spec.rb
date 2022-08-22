45885aa721bbde789f83331f5d1bd21c8b49525a
Ronald <rjskinnerc@unal.edu.co>
2022-08-22 16:01:42 -0500

    Add test to check for usernames of all users

--- spec/features/users_spec.rb
+++ spec/features/users_spec.rb
@@ -1,8 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe 'User listig path', type: :feature do
-  it 'it should render the users index page' do 
+  before do
+    @user = User.create(name: 'Hadi', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'something.', posts_counter: 1)
+    @user2 = User.create(name: 'Jafari', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'something.', posts_counter: 1)
+  end
+  it 'should show the username of all existing users' do 
     visit users_path
-    expect(page).to have_content('Users')
+    expect(page).to have_content('Hadi')
+    expect(page).to have_content('Jafari')
   end
 end

cb58c33f3afc4a626f7db00d4c2dae8d8461fd29
Abdul Hadi Jafari <abdulhadijafari2015@gmail.com>
2022-08-23 01:58:41 +0430

    Add test I can see the profile picture for each user.

--- spec/features/users_spec.rb
+++ spec/features/users_spec.rb
@@ -2,12 +2,19 @@ require 'rails_helper'
 
 RSpec.describe 'User listig path', type: :feature do
   before do
-    @user = User.create(name: 'Hadi', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'something.', posts_counter: 1)
-    @user2 = User.create(name: 'Jafari', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'something.', posts_counter: 1)
+    @user = User.create(id: 1, name: 'Hadi', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'something.',
+                        posts_counter: 1)
+    @user2 = User.create(id: 2, name: 'Jafari', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'something.',
+                         posts_counter: 1)
   end
-  it 'should show the username of all existing users' do 
+  it 'should show the username of all existing users' do
     visit users_path
     expect(page).to have_content('Hadi')
     expect(page).to have_content('Jafari')
+  end
+  it 'should show the profile picture for each user.' do
+    visit users_path
+    expect(page.find('#Hadi-img-1')['src']).to have_content('https://unsplash.com/photos/F_-0BxGuVvo')
+    expect(page.find('#Jafari-img-2')['src']).to have_content('https://unsplash.com/photos/F_-0BxGuVvo')
   end
 end

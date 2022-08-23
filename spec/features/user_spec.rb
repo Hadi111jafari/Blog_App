7159ce7ddfed78549d91d280df272bc36c17acc1
Ronald <rjskinnerc@unal.edu.co>
2022-08-23 10:22:04 -0500

    Add test to check for the user's first 3 posts

--- spec/features/user_spec.rb
+++ spec/features/user_spec.rb
@@ -4,6 +4,11 @@ RSpec.describe 'User listig path', type: :feature do
   before do
     @user = User.create(id: 1, name: 'Hadi', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'This is my bio.',
                         posts_counter: 0)
+    Post.create(author: @user, title: 'Post 1', text: 'Lorem ipsum dolor sit amet', comments_counter: 0, likes_counter: 0)
+    Post.create(author: @user, title: 'Post 2', text: 'Lorem ipsum dolor sit amet', comments_counter: 0, likes_counter: 0)
+    Post.create(author: @user, title: 'Post 3', text: 'Lorem ipsum dolor sit amet', comments_counter: 0, likes_counter: 0)
+    Post.create(author: @user, title: 'Post 4', text: 'Lorem ipsum dolor sit amet', comments_counter: 0, likes_counter: 0)
+    Post.create(author: @user, title: 'Post 5', text: 'Lorem ipsum dolor sit amet', comments_counter: 0, likes_counter: 0)
   end
 
   it 'should show the user\'s profile picture.' do
@@ -18,11 +23,19 @@ RSpec.describe 'User listig path', type: :feature do
 
   it 'should show the user\'s number of posts.' do
     visit user_path(@user)
-    expect(page).to have_content('Number of posts: 0')
+    expect(page).to have_content('Number of posts: 5')
   end
 
   it 'should show the user\'s bio.' do
     visit user_path(@user)
     expect(page).to have_content('This is my bio.')
+  end
+
+  it 'should show the user\'s first 3 posts.' do
+    visit user_path(@user)
+    expect(page).to have_content('Post 5')
+    expect(page).to have_content('Post 4')
+    expect(page).to have_content('Post 3')
+    expect(page).to_not have_content('Post 2')
   end
 end
